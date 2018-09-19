9ad05c9d2838dfcaaaee490d199af025fb191cc0
github username <github email address>
2018-09-19 12:03:29 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+
+  def bark
+    puts "Woof!"
+  end
+
+  def sit
+    puts "The Dog is sitting"
+  end
+
+end
+
+#Under your Dog class definition, create three dogs in local variables, fido, snoopy, and lassie.
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
